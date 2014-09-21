8d22cdd08a6fb85e5e1711d2f21656d6a938a640
Marcz Hermo <marcz@lab1521.com>
2014-09-21 18:20:05 +1200

    customized files for my end use

--- README.md
+++ README.md
@@ -4,3 +4,15 @@ Gulp-Bower-Starter
 Gulp, Bower, Bootstrap Sass, FontAwesome Starter
 
 Based on Eric Barnes tutorial [Setting up Gulp, Bower, Bootstrap Sass, & FontAwesome](http://ericlbarnes.com/setting-gulp-bower-bootstrap-sass-fontawesome/)
+
+mkdir testproject && cs testproject
+bower init
+bower install bootstrap-sass-official --save
+bower install fontawesome --save
+cat > package.json
+{}
+^C
+cat package.json
+npm install gulp gulp-ruby-sass gulp-notify gulp-autoprefixer gulp-bower --save-dev
+
+#create gulpfile.js and edit, see gulpfile.js
---
+++ gulpfile.js
@@ -0,0 +1,72 @@
+// Basic Gulp File
+//
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass')
+    autoprefix = require('gulp-autoprefixer')
+    notify = require("gulp-notify")
+    bower = require('gulp-bower');
+
+var config = {
+    sassPath: './resources/sass',
+    bowerDir: './bower_components'
+}
+
+gulp.task('bower', function() {
+    return bower()
+        .pipe(gulp.dest(config.bowerDir))
+});
+
+gulp.task('icons', function() {
+    return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*')
+        .pipe(gulp.dest('./public/fonts'));
+});
+
+gulp.task('fontawesomecss', function() {
+    return gulp.src(config.bowerDir + '/fontawesome/css/**.*')
+        .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('glyphicons', function() {
+    return gulp.src(config.bowerDir + '/bootstrap-sass-official/assets/fonts/bootstrap/**.*')
+        .pipe(gulp.dest('./public/fonts'));
+});
+
+gulp.task('fontawesome', function() {
+    return gulp.src(config.bowerDir + '/fontawesome/scss/font-awesome.scss')
+        .pipe(sass({
+            style: 'compressed',
+            loadPath: [
+                config.bowerDir + '/fontawesome/scss',
+            ]
+        })
+            .on("error", notify.onError(function (error) {
+                return "Error: " + error.message;
+            })))
+        .pipe(autoprefix('last 2 version'))
+        .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('css', function() {
+    return gulp.src(config.sassPath + '/style.scss')
+        .pipe(sass({
+            style: 'compressed',
+            loadPath: [
+                './resources/sass',
+                config.bowerDir + '/bootstrap-sass-official/assets/stylesheets',
+            ]
+        })
+            .on("error", notify.onError(function (error) {
+                return "Error: " + error.message;
+            })))
+        .pipe(autoprefix('last 2 version'))
+        .pipe(gulp.dest('./public/css'));
+});
+
+
+
+// Rerun the task when a file changes
+gulp.task('watch', function() {
+    gulp.watch(config.sassPath + '/**/*.scss', ['css']);
+});
+
+gulp.task('default', ['bower', 'icons', 'fontawesomecss', 'glyphicons', 'css']);
